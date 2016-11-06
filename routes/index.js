4a241a46aad47691e73a20a1cb02ee46d4823a88
Jimmy Weibel Rasmussen <jwe@danskecommodities.com>
2016-11-06 07:50:22 +0100

    bot will now echo messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,5 +34,20 @@ router.get('/webhook', function (req, res) {
     res.send('Error, wrong validation token');
   }
 });
+
+router.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+    }
+  }
+  res.sendStatus(200)
+})
+
+var token = "EAAVUOFvmWt4BABZA6SRsnvbnqZARCPopSM0Ayryd9GwJX284ZBZA1ZBF7x7ZA6nE5XqZAW1bGLSxYG4nzCn8bZBCpUb4kGGQXH0oHW1IsSxA0hZBgf9bEY9llfjtmA9RiLFfcRhZCWkKgdn0sGgYMz80N6rFbPKnLGDOJ3eaH8irRn3gZDZD"
 
 module.exports = router;
